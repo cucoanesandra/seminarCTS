29d7b75bf3455784e1a0a3da9aff7323ff1ff006
Andra <Andra@DESKTOP-Q8CJ8U9>
2021-02-25 08:56:08 +0200

    Animal Zoo

---
+++ seminar1_cts/src/classes/pack/Zoo.java
@@ -0,0 +1,63 @@
+package classes.pack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+
+	
+	private List<Animal> animals=new ArrayList<>(); 
+	private Zookeeper zookeeper;
+	
+	public Zoo() {
+		super();
+		animals=new ArrayList<>();
+		zookeeper=new Zookeeper("Gigi");
+	}
+	
+	public Zoo(List<Animal> animals, Zookeeper zookeeper) {
+		super();
+		this.animals = animals;
+		this.zookeeper = zookeeper;
+	}
+
+
+
+	public List<Animal> getAnimals() {
+		return animals;
+	}
+
+
+
+	public void setAnimals(List<Animal> animals) {
+		this.animals = animals;
+	}
+
+
+
+	public Zookeeper getZookeeper() {
+		return zookeeper;
+	}
+
+
+
+	public void setZookeeper(Zookeeper zookeeper) {
+		this.zookeeper = zookeeper;
+	}
+
+
+
+	public void feedAllAnimals()
+	{
+		for(Animal a: animals)
+		{
+			zookeeper.feed(a);
+		}
+	}
+	
+	public void addAnimal(Animal a)
+	{
+		animals.add(a);
+	}
+	
+}
--- seminar1_cts/src/main/pack/Main.java
+++ seminar1_cts/src/main/pack/Main.java
@@ -1,8 +1,10 @@
 package main.pack;
+import classes.pack.*;
 
 public class Main {
-
 	public static void main(String[] args) {
-		
+		Zookeeper keeper=new Zookeeper("Dani");
+		Zoo zoo=new Zoo();
+		zoo.setZookeeper(keeper);
 	}
 }
