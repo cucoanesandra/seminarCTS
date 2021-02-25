21f4395bb3dfb7fd34bd3dfa0f5f233ac39e249e
Andra <Andra@DESKTOP-Q8CJ8U9>
2021-02-25 09:13:31 +0200

    second change Animal

--- seminar1_cts/src/main/pack/Main.java
+++ seminar1_cts/src/main/pack/Main.java
@@ -6,5 +6,21 @@ public class Main {
 		Zookeeper keeper=new Zookeeper("Dani");
 		Zoo zoo=new Zoo();
 		zoo.setZookeeper(keeper);
+		
+		Giraffe giraffe=new Giraffe("Girafa", 10);
+		Giraffe secondgiraffe=new Giraffe("Girafa2", 15, 200);
+		
+		zoo.addAnimal(giraffe);
+		zoo.addAnimal(secondgiraffe);
+		
+		zoo.feedAllAnimals();
+		
+		Zebra z1=new Zebra("Zebra1", 7);
+		Zebra z2=new Zebra("Zebra 2", 9, 132);
+		
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
+		
+		zoo.feedAllAnimals();
 	}
 }
